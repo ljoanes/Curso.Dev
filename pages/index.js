bdc246470bc67b75c457f37ab8b73142b19f70c2
Leonardo Joanes da Silva <69224450+Leonardo-Joanes@users.noreply.github.com>
2023-08-04 12:42:32 +0000

    Formatando com Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho, pretinha! S2</h1>;
+  return (
+    <h1>
+      Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
+      pretinha! S2
+    </h1>
+  );
 }
 
 export default Home;
