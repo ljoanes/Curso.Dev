d1eecf1b93021a2fa2f9e9985344289cca1e3bc0
Leonardo Joanes da Silva <69224450+Leonardo-Joanes@users.noreply.github.com>
2023-08-05 11:48:56 +0000

    Formatando o arquivo

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,13 @@ function Home() {
         Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
         pretinha! S2
       </h1>
-      <img className={style.two} src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e%5Drecadinho-abra%C3%A7o-facebook-chambolinho.jpg" alt="Ursinho Pooh e Tigrão" height="500" width="366.875" />
+      <img
+        className={style.two}
+        src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e%5Drecadinho-abra%C3%A7o-facebook-chambolinho.jpg"
+        alt="Ursinho Pooh e Tigrão"
+        height="500"
+        width="366.875"
+      />
     </>
   );
 }
