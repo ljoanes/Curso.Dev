d891ac6a3fec1b38da53823a46cac2db87d5815b
Leonardo Joanes da Silva <69224450+Leonardo-Joanes@users.noreply.github.com>
2023-08-05 11:29:59 +0000

    Testando CSS Modules

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import style from './style.module.css';
+
 function Home() {
   return (
-    <h1>
-      Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
-      pretinha! S2
-    </h1>
+    <>
+      <h1 className={style.one}>
+        Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
+        pretinha! S2
+      </h1>
+      <img className={style.two} src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e%5Drecadinho-abra%C3%A7o-facebook-chambolinho.jpg" alt="Ursinho Pooh e Tigrão" height="500" width="366.875" />
+    </>
   );
 }
 
