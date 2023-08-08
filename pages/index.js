702c895cd8f7099fc5a2f96f47394e6a9665511c
Leonardo Joanes <leonardojoanes@gmail.com>
2023-08-08 12:04:54 -0300

    Acréscimo de prettierrc.json

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import style from './style.module.css';
 function Home() {
   return (
-    <section>
+    <section className={style.two}>
       <h1 className={style.one}>
         Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
         pretinha! S2
       </h1>
       <img
-        className={style.two}
         src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e%5Drecadinho-abra%C3%A7o-facebook-chambolinho.jpg"
         alt="Ursinho Pooh e Tigrão"
         height="500"
