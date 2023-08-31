1d9f5e0916049be06523654f6babfce897dba3d7
Leonardo Joanes <leonardojoanes@gmail.com>
2023-08-31 20:14:04 -0300

    Alterações no texto

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,18 @@ function Home() {
   return (
     <section className={style.two}>
       <h1 className={style.one}>
-        Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
-        pretinha! S2
+        Você me parou no dia 30/08 e não soltou minha mão. Tática velha de vendedora da 13 de maio em Campinas, não é mesmo? Hoje eu quero te parar pra dizer que realmente meu dia ficou melhor depois de soltar da sua mão e por três vezes voltar a segurar porque a conversa estava agradável e, mesmo entendo das táticas, eu me deixei levar porque a conversa estava gostosa.
+
+        Sabia que eu voltei lá pra tentar falar contigo e pegar seu número? Bolei um beck pr'um parceiro no Terminal Mercado e saí correndo pra ver se te encontrava na frente da JP como aconteceu uma vez. Não sei se vai lembrar.
+
+        Bem, que bom que vc me parou ali ontem. Foi a melhor abordagem que tiveram comigo! Se eu não tivesse o plano e os dentes certin' eu teria te acompanhado.
+
+        Tu é boa no que faz e deixou o dia de alguém melhor depois de ontem!
+
+        Só agradece, Carol Ketlym! :D
       </h1>
       <img
-        src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e%5Drecadinho-abra%C3%A7o-facebook-chambolinho.jpg"
+        src="https://pm1.aminoapps.com/6887/61d4ad28681d8b25d033be14296efb5736583c22r1-640-709v2_uhq.jpg"
         alt="Ursinho Pooh e Tigrão"
         height="500"
         width="366.875"
