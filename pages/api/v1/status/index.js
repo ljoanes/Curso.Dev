f814d0a2867e016a73b1117456da69dc8356379f
Leonardo Joanes <leonardojoanes@gmail.com>
2023-10-22 11:51:39 -0300

    Dia 18 iniciado

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).send({ chave: 'valor', Nome: 'Leonardo Joanes, o SwordDreamer' });
+import database from '../../../../infra/database.js';
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 AS "sum";');
+  console.log(result.rows);
+  response
+    .status(200)
+    .send({ chave: 'valor', Nome: 'Leonardo Joanes, o SwordDreamer' });
 }
 
 export default status;
