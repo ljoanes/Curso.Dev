74a41ebafa2024bd70eecb4caa3b0598c2c2b804
Leonardo Joanes <leonardojoanes@gmail.com>
2023-11-06 20:52:06 -0300

    Finalização Dia 18

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import style from './style.module.css';
 function Home() {
   return (
-    <section className={style.two}>
+    <section className={style.two} >
       <h1 className={style.one}>
-        Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho,
-        pretinha! S2
+        `Já te falei hoje que você é linda, Thais? Bom dia e bom trabalho, pretinha! S2`
       </h1>
       <img
-        src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e%5Drecadinho-abra%C3%A7o-facebook-chambolinho.jpg"
-        alt="Ursinho Pooh e Tigrão"
+        src="https://3.bp.blogspot.com/-qO9p4D-TD1s/Uesgql7hdrI/AAAAAAAAboE/_KCrqgi_96w/s1600/e]recadinho-abraço-facebook-chambolinho.jpg"
+        alt="Ursinho Pooh e Tigrão e Chambolinho"
         height="500"
         width="366.875"
       />
-    </section>
+    </section >
   );
 }
 
